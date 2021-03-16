1d3ddffcc576605c94fd8a7807d5b6caa15dcffd
‘markhend’ <markhend@gmail.com>
2021-03-16 15:16:59 -0500

    go 29

--- go/29.go
+++ go/29.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"math/big"
 )
 
 func main() {
 	h := make(map[int]int)
-	a := new(math.big.Int)
-	b := new(math.big.Int)
+	a := new(big.Int)
+	b := new(big.Int)
+	// Next 2 lines added to let compile
+	a = b
+	fmt.Println(a)
 	for i := 0; i < 100; i++ {
 		for j := 0; j < 100; j++ {
-			t = i + j
+			t := i + j
 			h[t] = t
 		}
 	}
